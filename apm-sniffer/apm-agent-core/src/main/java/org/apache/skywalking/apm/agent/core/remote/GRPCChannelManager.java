393aa8db04cc3a515d332478461b5eb9654c1ed5
朱伟 <zhuwei@tuhu.cn>
2021-03-02 01:52:43 +0800

    docs: GRPCChannelManager建立连接

--- apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/remote/GRPCChannelManager.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/remote/GRPCChannelManager.java
@@ -21,18 +21,6 @@ package org.apache.skywalking.apm.agent.core.remote;
 import io.grpc.Channel;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
-
-import java.net.InetAddress;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
 import org.apache.skywalking.apm.agent.core.boot.BootService;
 import org.apache.skywalking.apm.agent.core.boot.DefaultImplementor;
 import org.apache.skywalking.apm.agent.core.boot.DefaultNamedThreadFactory;
@@ -41,8 +29,18 @@ import org.apache.skywalking.apm.agent.core.logging.api.ILog;
 import org.apache.skywalking.apm.agent.core.logging.api.LogManager;
 import org.apache.skywalking.apm.util.RunnableWithExceptionProtection;
 
+import java.net.InetAddress;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 import static org.apache.skywalking.apm.agent.core.conf.Config.Collector.IS_RESOLVE_DNS_PERIODICALLY;
 
+/**
+ * grpc连接管理器，持有有效的channel连接，并支持短线重连和状态改变触发监听器回调
+ */
 @DefaultImplementor
 public class GRPCChannelManager implements BootService, Runnable {
     private static final ILog LOGGER = LogManager.getLogger(GRPCChannelManager.class);
@@ -63,19 +61,22 @@ public class GRPCChannelManager implements BootService, Runnable {
 
     @Override
     public void boot() {
+        // 指定oap服务端地址 collector.backend_service=${SW_AGENT_COLLECTOR_BACKEND_SERVICES:127.0.0.1:11800}
         if (Config.Collector.BACKEND_SERVICE.trim().length() == 0) {
             LOGGER.error("Collector server addresses are not set.");
             LOGGER.error("Agent will not uplink any data.");
             return;
         }
+        // oap服务端地址列表
         grpcServers = Arrays.asList(Config.Collector.BACKEND_SERVICE.split(","));
+        // 默认每隔30秒执行任务，检查是否需要重新建立grpc连接
         connectCheckFuture = Executors.newSingleThreadScheduledExecutor(
-            new DefaultNamedThreadFactory("GRPCChannelManager")
+                new DefaultNamedThreadFactory("GRPCChannelManager")
         ).scheduleAtFixedRate(
-            new RunnableWithExceptionProtection(
-                this,
-                t -> LOGGER.error("unexpected exception.", t)
-            ), 0, Config.Collector.GRPC_CHANNEL_CHECK_INTERVAL, TimeUnit.SECONDS
+                new RunnableWithExceptionProtection(
+                        this,
+                        t -> LOGGER.error("unexpected exception.", t)
+                ), 0, Config.Collector.GRPC_CHANNEL_CHECK_INTERVAL, TimeUnit.SECONDS
         );
     }
 
@@ -98,6 +99,7 @@ public class GRPCChannelManager implements BootService, Runnable {
     @Override
     public void run() {
         LOGGER.debug("Selected collector grpc service running, reconnect:{}.", reconnect);
+        // 可配置定期重新解析DNS配置
         if (IS_RESOLVE_DNS_PERIODICALLY && reconnect) {
             String backendService = Config.Collector.BACKEND_SERVICE.split(",")[0];
             try {
@@ -119,27 +121,32 @@ public class GRPCChannelManager implements BootService, Runnable {
             if (grpcServers.size() > 0) {
                 String server = "";
                 try {
+                    // 随机选取一个oap节点进行连接
                     int index = Math.abs(random.nextInt()) % grpcServers.size();
+                    // 重连节点不是上一次选取的节点
                     if (index != selectedIdx) {
                         selectedIdx = index;
 
                         server = grpcServers.get(index);
                         String[] ipAndPort = server.split(":");
 
+                        // 避免重新连接的时候GRPCChannel没有被释放造成内存泄露
                         if (managedChannel != null) {
                             managedChannel.shutdownNow();
                         }
 
+                        // 创建channel连接
                         managedChannel = GRPCChannel.newBuilder(ipAndPort[0], Integer.parseInt(ipAndPort[1]))
-                                                    .addManagedChannelBuilder(new StandardChannelBuilder())
-                                                    .addManagedChannelBuilder(new TLSChannelBuilder())
-                                                    .addChannelDecorator(new AgentIDDecorator())
-                                                    .addChannelDecorator(new AuthenticationDecorator())
-                                                    .build();
+                                .addManagedChannelBuilder(new StandardChannelBuilder())
+                                .addManagedChannelBuilder(new TLSChannelBuilder())
+                                .addChannelDecorator(new AgentIDDecorator())
+                                .addChannelDecorator(new AuthenticationDecorator())
+                                .build();
                         notify(GRPCChannelStatus.CONNECTED);
                         reconnectCount = 0;
                         reconnect = false;
                     } else if (managedChannel.isConnected(++reconnectCount > Config.Agent.FORCE_RECONNECTION_PERIOD)) {
+                        // 是上一次选取的节点，并且达到指定次数就会重新创建连接
                         // Reconnect to the same server is automatically done by GRPC,
                         // therefore we are responsible to check the connectivity and
                         // set the state and notify listeners
@@ -155,8 +162,8 @@ public class GRPCChannelManager implements BootService, Runnable {
             }
 
             LOGGER.debug(
-                "Selected collector grpc service is not available. Wait {} seconds to retry",
-                Config.Collector.GRPC_CHANNEL_CHECK_INTERVAL
+                    "Selected collector grpc service is not available. Wait {} seconds to retry",
+                    Config.Collector.GRPC_CHANNEL_CHECK_INTERVAL
             );
         }
     }
@@ -193,8 +200,8 @@ public class GRPCChannelManager implements BootService, Runnable {
         if (throwable instanceof StatusRuntimeException) {
             StatusRuntimeException statusRuntimeException = (StatusRuntimeException) throwable;
             return statusEquals(
-                statusRuntimeException.getStatus(), Status.UNAVAILABLE, Status.PERMISSION_DENIED,
-                Status.UNAUTHENTICATED, Status.RESOURCE_EXHAUSTED, Status.UNKNOWN
+                    statusRuntimeException.getStatus(), Status.UNAVAILABLE, Status.PERMISSION_DENIED,
+                    Status.UNAUTHENTICATED, Status.RESOURCE_EXHAUSTED, Status.UNKNOWN
             );
         }
         return false;
